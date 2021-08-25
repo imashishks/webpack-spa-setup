78f2eeef2068ee0c61f4368ddc8a7bfe18b585fc
Ashish Kumar <sayhitoak@gmail.com>
2021-08-26 01:38:43 +0530

    Added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     mode: 'development',
+    module:{
+        rules:[{
+            test:/\.js$/,
+            exclude: /node_modules/,
+            use:{
+                loader:'babel-loader'
+            }
+        }]
+    },
+    devtool: false,
     devServer:{
         contentBase: '/dist'
     }
